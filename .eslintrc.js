3f302a0f3802b7472a386ea51153a3f92725d217
Eleven <jun.xi@ximalaya.com>
2021-05-06 16:51:16 +0800

    chore: modify eslint & prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,33 @@
 /**
  * https://cn.eslint.org/docs/user-guide/configuring
  */
+
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    // 'prettier/@typescript-eslint',
-    // 'plugin:prettier/recommended',
+    // https://github.com/umijs/fabric
+    require.resolve('@umijs/fabric/dist/eslint'),
   ],
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-    commonjs: true,
-    jest: true,
-  },
-  globals: {},
   rules: {
-    // 'off' - 0, 'warn' - 1, 'error' - 2
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/explicit-module-boundary-types': 0,
-    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/consistent-type-definitions': 0,
+    '@typescript-eslint/no-invalid-this': 0,
+    '@typescript-eslint/consistent-type-imports': 0,
+    'no-underscore-dangle': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-dynamic-require': 0,
+    'react/sort-comp': 0,
+    'jsx-a11y/aria-role': 0,
+    'jsx-a11y/alt-text': 0,
+    'jsx-a11y/anchor-has-content': 0,
+    'jsx-a11y/control-has-associated-label': 0,
+    'no-param-reassign': 0,
+    'prefer-object-spread': 0,
+    'consistent-return': 0,
+    'react/button-has-type': 0,
+    'react/react-in-jsx-scope': 0,
+    'prefer-destructuring': 0,
+    'array-callback-return': 0,
+    'no-plusplus': 0,
+    'no-nested-ternary': 0,
   },
-}
+};
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,10 @@
+/**
+ * https://prettier.io/docs/en/options.html
+ */
 module.exports = {
   arrowParens: 'avoid',
   trailingComma: 'es5',
   singleQuote: true,
   semi: true,
-}
+  tabWidth: 2,
+};
