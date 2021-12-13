0e5a56320e53a4accacd4995d12d778500978500
jun.xi <jun.xi@ximalaya.com>
2021-12-13 20:06:35 +0800

    chore: update babel config

--- rollup.config.js
+++ rollup.config.js
@@ -48,7 +48,61 @@ const plugins = [
   isProd && filesize(),
 ].filter(Boolean);
 
-export default [
+/**
+ * babel 配置
+ * @param {String} type 'cjs' | 'esm'
+ */
+function createBabelConfig(type) {
+  return {
+    assumptions: {
+      /**
+       * https://babeljs.io/docs/en/assumptions#setpublicclassfields
+       *
+       * 装饰器的 legancy: true，依赖此配置
+       *  - https://babeljs.io/docs/en/babel-plugin-proposal-decorators#legacy
+       */
+      setPublicClassFields: true,
+    },
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: false,
+        },
+      ],
+      '@babel/preset-typescript',
+      '@babel/preset-react',
+    ],
+    plugins: [
+      [
+        '@babel/plugin-transform-runtime',
+        {
+          corejs: {
+            version: 3,
+            proposals: true,
+          },
+          useESModules: type === 'esm' ? true : 'auto',
+        },
+      ],
+      [
+        // @babel/plugin-proposal-decorators 需要在 @babel/plugin-proposal-class-properties 之前
+        '@babel/plugin-proposal-decorators',
+        {
+          legacy: true, // 推荐
+        },
+      ],
+      ['@babel/plugin-proposal-class-properties'],
+    ],
+  };
+}
+
+/**
+ * rollup 配置
+ * @param {String} type 'esm' | 'cjs' | 'umd'
+ */
+function createRollupConfig(type) {}
+
+[
   {
     input: `src/index.ts`,
     output: [
